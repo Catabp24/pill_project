9fdeba36716b1e0b87e238daf73ce4ca771b7375
Catalina Betancourt <betancourtc836@gmail.com>
2024-08-28 17:53:42 -0500

    se modifican codigos de status en algunas solicitudes

--- src/main/java/com/pill/registro/controller/UsuarioController.java
+++ src/main/java/com/pill/registro/controller/UsuarioController.java
@@ -2,6 +2,8 @@ package com.pill.registro.controller;
 
 import com.pill.registro.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,7 +15,7 @@ public class UsuarioController {
     private UsuarioService usuarioService;
 
     @PostMapping("/nuevo")
-
+    @ResponseStatus(HttpStatus.CREATED)
     public Usuario newUsuario(@RequestBody Usuario newUsuario) {
         return this.usuarioService.newUsuario(newUsuario);
 
@@ -26,13 +28,20 @@ public class UsuarioController {
 
     }
     @PostMapping("/modificar")
+    @ResponseStatus(HttpStatus.CREATED)
     public Usuario updateUsuario(@RequestBody Usuario usuario) {
         return this.usuarioService.modifyUsuario(usuario);
 
     }
-    @PostMapping(value = "/{id}")
-    public boolean deleteUsuario(@PathVariable(value ="id")long id){
-        return this.usuarioService.deleteUsuario(id);
+    @DeleteMapping(value = "/usuarios/{id}")
+    public ResponseEntity<Void> deleteUsuario(@PathVariable(value = "id") long id) {
+        boolean isDeleted = this.usuarioService.deleteUsuario(id);
+
+        if (isDeleted) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // 204 No Content
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND); // 404 Not Found si el usuario no fue encontrado
+        }
     }
 
 
